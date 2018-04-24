5816ec0e209e847a1c4e9d07233a9dc5604cc3f5
ddxiao <809660773@qq.com>
2018-04-24 14:57:12 +0800

    添加 随机user-agent、代理IP 反反爬虫措施

--- ArticleSpider/middlewares.py
+++ ArticleSpider/middlewares.py
@@ -4,8 +4,9 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+from fake_useragent import UserAgent
 from scrapy import signals
+from ArticleSpider.utils.crawl_xici_ip import GetIP
 
 
 class ArticlespiderSpiderMiddleware(object):
@@ -101,3 +102,28 @@ class ArticlespiderDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class RandomUserAgentMiddlware(object):
+    # 随机更换user-agent
+    def __init__(self, crawler):
+        super(RandomUserAgentMiddlware, self).__init__()
+        self.ua = UserAgent()
+        self.ua_type = crawler.settings.get("RANDOM_UA_TYPE", "random")
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler)
+
+    def process_request(self, request, spider):
+        def get_ua():
+            return getattr(self.ua, self.ua_type)
+
+        request.headers.setdefault('User-Agent', get_ua())
+
+
+class RandomProxyMiddleware(object):
+    # 设置代理
+    def process_request(self, request,spider):
+        get_ip = GetIP()
+        request.meta['proxy'] = get_ip.get_random_ip()
